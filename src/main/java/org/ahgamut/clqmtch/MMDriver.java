e3a051fb63919f7df09525635c21db37c5d10e82
Gautham <ahgamut@gmail.com>
2024-01-22 15:38:53 -0600

    basic project setup

---
+++ src/main/java/org/ahgamut/clqmtch/MMDriver.java
@@ -0,0 +1,16 @@
+package org.ahgamut.clqmtch;
+
+/**
+ * Hello world!
+ *
+ */
+public class MMDriver
+{
+    public static void main(String[] args)
+    {
+        System.out.println( "Hello World!" );
+        for (String arg: args) {
+            System.out.println(arg);
+        }
+    }
+}
---
+++ src/test/java/org/ahgamut/clqmtch/MMDriverTest.java
@@ -0,0 +1,20 @@
+package org.ahgamut.clqmtch;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple MMDriver.
+ */
+public class MMDriverTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
